f1e0f72387eefff9ac7c56e42041eae8727a2094
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-10-25 20:24:01 -0400

    rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rubynewb::Application.routes.draw do
-  resources :users
 
   get "welcome/index"
   get "welcome/about"
-
   get 'live' => 'live_video#index'
 
+  resources :users
   resources :blog_posts, only: [:create, :index, :edit, :update]
 
   root :to => "welcome#index"

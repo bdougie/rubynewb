f40d16af72f01dc23edd5cac1923a07ff269a019
Brian /bdougie/ Douglas <brian.douglas@izea.com>
2014-10-25 20:26:13 -0400

    add minitest helpers for integration testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,9 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'minitest/autorun'
 require 'pry'
+require 'capybara/rails'
+require "active_support/testing/setup_and_teardown"
+
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
@@ -14,4 +17,16 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+class IntegrationTest < MiniTest::Spec
+  include Rails.application.routes.url_helpers
+  include Capybara::DSL
+  register_spec_type(/integration$/, self)
+end
+
+class HelperTest < MiniTest::Spec
+  include ActiveSupport::Testing::SetupAndTeardown
+  include ActionView::TestCase::Behavior
+  register_spec_type(/Helper$/, self)
 end
